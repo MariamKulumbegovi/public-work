dfc03bbea557bbc74b82b2365d0fa211362d44ef
MariamK <78789152+MariamKulumbegovi@users.noreply.github.com>
2022-04-18 16:15:31 +0400

    created project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+export const Header = () => {
+  return (
+    <header className='header'>
+      <ul className='header-ul'>
+        <li>არქივი</li>
+        <li>ორგანიზაციის სახელი</li>
+      </ul>
+    </header>
+  )
+}
