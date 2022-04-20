0ad365834c9307d91cdf0ee5159e5d6ae4eef5c0
Sophio Makharadze <75243064+Sofi198818@users.noreply.github.com>
2022-04-20 17:31:02 +0400

    created Data for FAQ

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -28,6 +28,20 @@ export const Questions = () => {
               <Panel header={question.title} key={index}>
                 <p>{question.info}</p>
                 <p>{question.infoTwo}</p>
+                <ul>
+                  <li>{question.list1}</li>
+                  <li>{question.list2}</li>
+                  <li>{question.list3}</li>
+                  <li>{question.list4}</li>
+                  <li>{question.list5}</li>
+                  <li>{question.list6}</li>
+                  <li>{question.list7}</li>
+                  <p>{question.infoThree}</p>
+                  <p>{question.infoFour}</p>
+                  <li>{question.a}</li>
+                  <li>{question.b}</li>
+                  <a href="mailto:infosesa@moh.gov.ge">{question.mail}</a>
+                </ul>
               </Panel>
             ))}
           </Collapse>
