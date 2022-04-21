a75433f19b81145208b257f335b3a53b062ff3fa
MariamK <78789152+MariamKulumbegovi@users.noreply.github.com>
2022-04-21 10:17:19 +0400

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-
+import 'antd/dist/antd.css';
 // axios.defaults.baseURL = 'https://publicworkapi.moh.gov.ge/Test/api/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
